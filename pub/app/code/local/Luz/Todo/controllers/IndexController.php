9c58f53d5f89f109a85a6bbb14cc70ef477ddd38
Vernard Alexis Luz <vernard@luzcommerce.com>
2023-04-02 16:34:17 +0800

    Create controller for `/todo` URL

---
+++ pub/app/code/local/Luz/Todo/controllers/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Luz_Todo_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction() {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
