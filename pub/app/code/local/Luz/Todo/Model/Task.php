51cc8a820848fb34b9d870cbcb96760513540dc8
Vernard Alexis Luz <vernard@luzcommerce.com>
2023-04-08 17:32:09 +0800

    Add Task Model related classes and update saveAction in TodoController

---
+++ pub/app/code/local/Luz/Todo/Model/Task.php
@@ -0,0 +1,8 @@
+<?php
+
+class Luz_Todo_Model_Task extends Mage_Core_Model_Abstract
+{
+    public function _construct() {
+        $this->_init('luz_todo/task');
+    }
+}
--- pub/app/code/local/Luz/Todo/controllers/IndexController.php
+++ pub/app/code/local/Luz/Todo/controllers/IndexController.php
@@ -9,10 +9,15 @@ class Luz_Todo_IndexController extends Mage_Core_Controller_Front_Action
 
     public function saveAction() {
         if (! $this->getRequest()->isPost()) {
-            return; // Must redirect to home page.
+            return $this->_redirect('*/*/index');
         }
 
         $task_name = $this->getRequest()->getParam('task_name');
-        echo "You've entered: {$task_name}";
+
+        $taskModel = Mage::getModel('luz_todo/task');
+        $taskModel->setName($task_name);
+        $taskModel->save();
+
+        $this->_redirectReferer();
     }
 }
