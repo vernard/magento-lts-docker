6e8a80e235c79f94ee31e353810a6acef2dafde5
Vernard Alexis Luz <vernard@luzcommerce.com>
2023-04-08 16:20:13 +0800

    Added layout XML and simple form submission in Todo module

--- pub/app/code/local/Luz/Todo/controllers/IndexController.php
+++ pub/app/code/local/Luz/Todo/controllers/IndexController.php
@@ -6,4 +6,13 @@ class Luz_Todo_IndexController extends Mage_Core_Controller_Front_Action
         $this->loadLayout();
         $this->renderLayout();
     }
+
+    public function saveAction() {
+        if (! $this->getRequest()->isPost()) {
+            return; // Must redirect to home page.
+        }
+
+        $task_name = $this->getRequest()->getParam('task_name');
+        echo "You've entered: {$task_name}";
+    }
 }
